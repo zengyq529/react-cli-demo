caaa5d18d8c3045ea1e538b8349477f94db3a899
yuqinzeng <yuqinzeng@tencent.com>
2020-12-07 16:57:58 +0800

    update

--- src/pages/demopage/Index.jsx
+++ src/pages/demopage/Index.jsx
@@ -1,9 +1,18 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import AsyncApp from './components/AsyncApp'
+import configureStore from './model/configureStore'
+import { Provider } from 'react-redux'
+const store = configureStore()
 
 const DemoPage = () => {
-  return <div>demopage</div>
+  return (
+    <Provider store={store}>
+      <h1>redux 高级完整例子</h1>
+      <AsyncApp></AsyncApp>
+    </Provider>
+  )
 }
 
 
-export default DemoPage  
+export default DemoPage
